695c599ffa4ec133fef2a67a60ecba8527ac9b65
blake <blake6697@gmail.com>
2016-04-10 15:47:42 -0500

    made text boxes not ridiculously over clompicated. Implemented number boxes.

--- FinalGame_Beta/src/Screens/PlayScreen.java
+++ FinalGame_Beta/src/Screens/PlayScreen.java
@@ -93,7 +93,7 @@ public class PlayScreen implements Screen{// this screen is a test screen im usi
 			roomRep = new RoomRepository();
 			
 			//uses the world builder to generate a  world to display
-			world = worldBuilder.RoomBasedRandom(50)
+			world = worldBuilder.makeBaseRoom()
 					.buildWorld();
 		
 			cf = new CreatureFactory(world);
@@ -108,7 +108,7 @@ public class PlayScreen implements Screen{// this screen is a test screen im usi
 			
 			//initalize popuphandeler
 			popUp = new PopupHandeler(0, screenHeight-1, screenWidth, 6);
-			 popUp.text = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch! Beware the Jubjub bird, and shun The frumious Bandersnatch! He took his vorpal sword in hand; Long time the manxome foe he sought- So rested he by the Tumtum tree And stood awhile in thought And, as in uffish thought he stood, The Jabberwock, with eyes of flame, Came whiffling through the tulgey wood, And burbled as it came! One, two! One, two! And through and through The vorpal blade went snicker-snack! He left it dead, and with its head He went galumphing back. And hast thou slain the Jabberwock? Come to my arms, my beamish boy! O frabjous day! Callooh! Callay! He chortled in his joy. Twas brillig, and the slithy toves Did gyre and gimble in the wabe: All mimsy were the borogoves, And the mome raths outgrabe.";
+			// popUp.text = "Beware the Jabberwock, my son! The jaws that bite, the claws that catch! Beware the Jubjub bird, and shun The frumious Bandersnatch! He took his vorpal sword in hand; Long time the manxome foe he sought- So rested he by the Tumtum tree And stood awhile in thought And, as in uffish thought he stood, The Jabberwock, with eyes of flame, Came whiffling through the tulgey wood, And burbled as it came! One, two! One, two! And through and through The vorpal blade went snicker-snack! He left it dead, and with its head He went galumphing back. And hast thou slain the Jabberwock? Come to my arms, my beamish boy! O frabjous day! Callooh! Callay! He chortled in his joy. Twas brillig, and the slithy toves Did gyre and gimble in the wabe: All mimsy were the borogoves, And the mome raths outgrabe.";
 			//debug
 			
 		}
@@ -160,7 +160,7 @@ public class PlayScreen implements Screen{// this screen is a test screen im usi
 	   if (subscreen != null)
 		   subscreen.displayOutput(terminal);
 	  
-	popUp.display(terminal);
+	popUp.displayOutput(terminal);
 	}
 	
 	//Function to Display the tiles of the world
@@ -208,6 +208,10 @@ public class PlayScreen implements Screen{// this screen is a test screen im usi
 
 	@Override
 	public Screen respondToUserInput(KeyEvent key) {
+		if(this.popUp != null){
+			popUp.respondToUserInput(key);
+		}
+		
 		if (subscreen != null) {
 			subscreen = subscreen.respondToUserInput(key);
 		}else{
@@ -245,8 +249,7 @@ public class PlayScreen implements Screen{// this screen is a test screen im usi
 		case KeyEvent.VK_J: if (player.z >= 1){ player.z--; }
 		break;
 		default: break;
-		case KeyEvent.VK_T: popUp.text = "Hello"; break;
-		case KeyEvent.VK_ENTER: popUp.next(); break;
+		
 		}
 		
 	}
